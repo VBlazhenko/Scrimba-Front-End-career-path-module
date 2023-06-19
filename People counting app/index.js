202f69e4eebcd331e76b769ce87e6a45b256600f
Vladyslav Blazhenko <78543274+VBlazhenko@users.noreply.github.com>
2023-06-19 11:41:25 +0100

    Add files via upload
    added js incrementation to the web app

--- People counting app/index.js
+++ People counting app/index.js
@@ -1,1 +1,16 @@
-document.getElemendById("count-el").innerText = 5
+// document.getElemendById("count-el").innerText = 5
+
+// initialising the count to 0
+// listen for clicks on the increment button
+// increase the count variable
+// reflect the changes
+
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count = count + 1 
+    countEl.innerText = count
+    console.log(count )
+}
