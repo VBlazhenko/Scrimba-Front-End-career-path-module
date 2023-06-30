e6c3eacc05e3df3a611fa662a8db4c98f850c048
Vladyslav Blazhenko <78543274+VBlazhenko@users.noreply.github.com>
2023-06-30 16:39:39 +0100

    Add files via upload

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,18 +1,37 @@
 let firstCard = 10
 let secondCard = 4
+
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
+
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl = document.querySelector("#message-el") // comes in a form of an id
+let sumEl = document.querySelector("#sum-el") //comes in a form of an id
+let cardsEl = document.querySelector("#cards-el")
+
+function startGame() {
+    renderGame()
 }
 
-console.log(message)
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        message = "You've got Blackjack! "
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false
+    }
+}
+
+function newCard() {
+    let card = 6
+    sum += card
+    renderGame()
+}
