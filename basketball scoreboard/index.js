47bf55e856cd358490c3ac6e7e903958991f3188
Vladyslav Blazhenko <78543274+VBlazhenko@users.noreply.github.com>
2023-06-30 13:00:58 +0100

    Add files via upload
    finished the app

---
+++ basketball scoreboard/index.js
@@ -0,0 +1,38 @@
+
+let countEl = document.getElementById("sum-el-1")
+let countElement = document.getElementById("sum-el-2")
+let counth = 0
+let countg = 0
+
+
+function addoneh() {
+    counth += 1
+    countEl.textContent = counth
+}
+
+function addtwoh() {
+    counth += 2
+    countEl.textContent = counth
+}
+
+function addthreeh() {
+    counth += 3
+    countEl.textContent = counth
+}
+
+// GUESTS
+
+function addoneg() {
+    countg += 1
+    countElement.textContent = countg
+}
+
+function addtwog() {
+    countg += 2
+    countElement.textContent = countg
+}
+
+function addthreeg() {
+    countg += 3
+    countElement.textContent = countg
+}
